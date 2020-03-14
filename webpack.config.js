e71282f33d87efcfaf399ffa96e78760d13d0e01
Michal-Szczepaniak <m.szczepaniak.000@gmail.com>
2020-03-14 20:28:23 +0100

    Update packages

--- webpack.config.js
+++ webpack.config.js
@@ -14,19 +14,19 @@ Encore
     .autoProvidejQuery()
 
     // uncomment to define the assets of the project
-    // .addEntry('js/jquery.scrollex.min', './assets/js/jquery.scrollex.min.js')
-    // .addEntry('js/jquery.scrolly.min', './assets/js/jquery.scrolly.min.js')
-    // .addEntry('js/skel.min', './assets/js/skel.min.js')
-    // .addEntry('js/ie/respond.min', './assets/js/ie/respond.min.js')
-    // .addEntry('js/ie/html5shiv', './assets/js/ie/html5shiv.js')
-    // .addEntry('js/util', './assets/js/util.js')
-    // .addEntry('js/main', './assets/js/main.js')
-    //
-    // .addStyleEntry('css/main', './assets/css/main.css')
-    // .addStyleEntry('css/ie9', './assets/css/ie9.css')
-    // .addStyleEntry('css/ie8', './assets/css/ie8.css')
-    // .addStyleEntry('css/font-awesome.min', './assets/css/font-awesome.min.css')
+    .addEntry('js/jquery.scrollex.min', './assets/js/jquery.scrollex.min.js')
+    .addEntry('js/jquery.scrolly.min', './assets/js/jquery.scrolly.min.js')
+    .addEntry('js/skel.min', './assets/js/skel.min.js')
+    .addEntry('js/ie/respond.min', './assets/js/ie/respond.min.js')
+    .addEntry('js/ie/html5shiv', './assets/js/ie/html5shiv.js')
+    .addEntry('js/util', './assets/js/util.js')
+    .addEntry('js/main', './assets/js/main.js')
 
+    .addStyleEntry('css/main', './assets/css/main.css')
+    .addStyleEntry('css/ie9', './assets/css/ie9.css')
+    .addStyleEntry('css/ie8', './assets/css/ie8.css')
+    .addStyleEntry('css/font-awesome.min', './assets/css/font-awesome.min.css')
+    //
     // uncomment if you use Sass/SCSS files
     // .enableSassLoader()
 
